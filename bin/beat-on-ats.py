85d40164eb85b8af010c79758d05550254b0763e
Nick LeRoy <nleroy@qualys.com>
2013-07-01 12:42:08 -0500

    beat-on-ats: Simple fix

--- bin/beat-on-ats.py
+++ bin/beat-on-ats.py
@@ -330,6 +330,8 @@ class _Main( object ) :
                 return
             cmd = self.BuildCurlCmd( )
             while True :
+                if shutdown :
+                    break
                 if self._max_procs is None  or  len(self._children) < self._max_procs :
                     started = self.StartCmd( cmd, label="#%d"%proc, out=dev_null )
                     if started :
